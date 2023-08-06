e2418aae78d99e71cb15501faae114fc37ac2e9c
Anh Huy <lebuuanhhuyle@gmail.com>
2023-08-06 17:37:04 +0700

    inital release

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (env('APP_ENV') == 'Production') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
